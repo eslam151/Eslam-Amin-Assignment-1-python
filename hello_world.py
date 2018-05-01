bc98c0cc1adb7d780c1860df7e903c96bf8eb3af
Eslam Amin <eslamamin@Eslams-Air.localdomain>
2018-05-01 13:38:19 -0500

    Tip Calculator

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+Total_amount = float(input("Enter total amount"))
+tip_percantage_amount = float(input("Enter the tip percentage amount"))
+tip_amount= (tip_percantage_amount/100) * Total_amount
+
+print(tip_amount)
+
